b746077691a49b68be7cf4752c9f1c35a3e63858
Andreas Müller <taori@users.noreply.github.com>
2019-12-23 06:47:42 +0100

    WIP State

--- src/Amusoft.CodeAnalysis.Analyzers.Test/Tests/CodeGeneration/DelegateImplementationToFieldsTests.cs
+++ src/Amusoft.CodeAnalysis.Analyzers.Test/Tests/CodeGeneration/DelegateImplementationToFieldsTests.cs
@@ -164,8 +164,8 @@ namespace Amusoft.CodeAnalysis.Analyzers.Test.Tests.CodeGeneration
 			VerifyCSharpDiagnostic(test, expected);
 		}
 
-		//		[TestMethod]
-		public void NoFieldNoChange()
+		[TestMethod]
+		public void VerifyFullRewrite()
 		{
 			var test = @"
     using System;
@@ -177,8 +177,43 @@ namespace Amusoft.CodeAnalysis.Analyzers.Test.Tests.CodeGeneration
 
     namespace ConsoleApplication1
     {
-        class TypeName
-        {   
+        class TypeName : ICustomInterface
+        {
+            private ICollection<ICustomInterface> _disposables;
+
+            public void Method1(object p1)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Method1(string p1)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Method2(string p1, int p2)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Method2(string p1)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Method3()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface ICustomInterface
+        {
+            void Method1(object p1);
+            void Method1(string p1);
+            void Method2(string p1, int p2);
+            void Method2(string p1);
+            void Method3();
         }
     }";
 			var expected = new DiagnosticResult
@@ -205,8 +240,187 @@ namespace Amusoft.CodeAnalysis.Analyzers.Test.Tests.CodeGeneration
 
     namespace ConsoleApplication1
     {
-        class TypeName
-        {   
+        class TypeName : ICustomInterface
+        {
+            private ICollection<ICustomInterface> _disposables;
+
+            public void Method1(object p1)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method1(p1);
+                }
+            }
+
+            public void Method1(string p1)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method1(p1);
+                }
+            }
+
+            public void Method2(string p1, int p2)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method2(p1, p2);
+                }
+            }
+
+            public void Method2(string p1)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method2(p1);
+                }
+            }
+
+            public void Method3()
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method3();
+                }
+            }
+        }
+
+        public interface ICustomInterface
+        {
+            void Method1(object p1);
+            void Method1(string p1);
+            void Method2(string p1, int p2);
+            void Method2(string p1);
+            void Method3();
+        }
+    }";
+			VerifyCSharpFix(test, fixtest);
+		}
+
+		[TestMethod]
+		public void VerifyPartialRewrite()
+		{
+			var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName : ICustomInterface
+        {
+            private ICollection<ICustomInterface> _disposables;
+
+            public void Method1(object p1)
+            {
+            }
+
+            public void Method1(string p1)
+            {
+                var a = 1;
+            }
+
+            public void Method2(string p1, int p2)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Method2(string p1)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Method3()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public interface ICustomInterface
+        {
+            void Method1(object p1);
+            void Method1(string p1);
+            void Method2(string p1, int p2);
+            void Method2(string p1);
+            void Method3();
+        }
+    }";
+			var expected = new DiagnosticResult
+			{
+				Id = Analyzer.DiagnosticId,
+				Message = string.Format(Resources.DelegateImplementationToFieldAnalyzerMessageFormat),
+				Severity = DiagnosticSeverity.Info,
+				Locations =
+					new[]
+					{
+						new DiagnosticResultLocation("Test0.cs", 11, 15)
+					}
+			};
+
+			VerifyCSharpDiagnostic(test, expected);
+
+			var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName : ICustomInterface
+        {
+            private ICollection<ICustomInterface> _disposables;
+
+            public void Method1(object p1)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method1(p1);
+                }
+            }
+
+            public void Method1(string p1)
+            {
+                var a = 1;
+            }
+
+            public void Method2(string p1, int p2)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method2(p1, p2);
+                }
+            }
+
+            public void Method2(string p1)
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method2(p1);
+                }
+            }
+
+            public void Method3()
+            {
+                foreach (var item in _disposables)
+                {
+                    item.Method3();
+                }
+            }
+        }
+
+        public interface ICustomInterface
+        {
+            void Method1(object p1);
+            void Method1(string p1);
+            void Method2(string p1, int p2);
+            void Method2(string p1);
+            void Method3();
         }
     }";
 			VerifyCSharpFix(test, fixtest);
@@ -215,6 +429,11 @@ namespace Amusoft.CodeAnalysis.Analyzers.Test.Tests.CodeGeneration
 		protected override CodeFixProvider GetCSharpCodeFixProvider()
 		{
 			return new Fixer();
+		}
+
+		public void Bla()
+		{
+			throw new NotImplementedException();
 		}
 
 		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
