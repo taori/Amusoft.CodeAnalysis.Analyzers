1d365c09501cd5777f23502221940a6fac6a7ec2
Andreas Müller <taori@users.noreply.github.com>
2019-12-24 18:41:18 +0100

    pipeline debugging

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -14,13 +14,11 @@ Write-Host "Version: $version"
 Write-Host "Path to analyzer project: $analyzerProjectPath"
 Write-Host "Path to vsix project manifest file: $vsixManifestPath"
 
-Test-Path $analyzerProjectPath
-Test-Path $vsixManifestPath
-
 # Update NuGet package version
 $FullPath = $analyzerProjectPath
 Write-Host $FullPath
 [xml]$content = Get-Content $FullPath
+Write-Host $content
 $content.Project.PropertyGroup.PackageVersion = $version
 $content.Save($FullPath)
 
