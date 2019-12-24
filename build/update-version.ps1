7788648d76d66b9bf7ac8094b0e560b037e31280
Andreas Müller <taori@users.noreply.github.com>
2019-12-24 18:44:22 +0100

    pipeline debugging

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -4,7 +4,7 @@ $VerbosePreference="Continue"
 
 $version = $args[0]
 $analyzerProjectPath = $args[1]
-$vsixManifestPath = $args[2]
+$vsixSourceManifestPath = $args[2]
 
 if (!$version) {
     $version = "0.0.0"
@@ -12,7 +12,7 @@ if (!$version) {
 
 Write-Host "Version: $version"
 Write-Host "Path to analyzer project: $analyzerProjectPath"
-Write-Host "Path to vsix project manifest file: $vsixManifestPath"
+Write-Host "Path to vsix project manifest file: $vsixSourceManifestPath"
 
 # Update NuGet package version
 $FullPath = $analyzerProjectPath
@@ -23,7 +23,7 @@ $content.Project.PropertyGroup.PackageVersion = $version
 $content.Save($FullPath)
 
 # Update VSIX version
-$FullPath = $vsixManifestPath 
+$FullPath = $vsixSourceManifestPath
 Write-Host $FullPath
 [xml]$content = Get-Content $FullPath
 $content.PackageManifest.Metadata.Identity.Version = $version
