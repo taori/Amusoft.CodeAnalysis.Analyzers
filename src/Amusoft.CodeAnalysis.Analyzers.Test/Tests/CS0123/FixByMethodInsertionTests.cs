441b2dfdd733cbb586d7154ec9c9a104cc5b9cf3
Andreas Müller <taori@users.noreply.github.com>
2020-01-12 21:21:12 +0100

    refactoring

--- src/Amusoft.CodeAnalysis.Analyzers.Test/Tests/CS0123/FixByMethodInsertionTests.cs
+++ src/Amusoft.CodeAnalysis.Analyzers.Test/Tests/CS0123/FixByMethodInsertionTests.cs
@@ -1,23 +1,30 @@
-﻿using System.Threading.Tasks;
-using Amusoft.CodeAnalysis.Analyzers.CS0123;
+﻿// // Copyright 2020 Andreas Müller
+// // This file is a part of Amusoft.CodeAnalysis.Analyzers and is licensed under Apache 2.0
+// // See https://github.com/taori/Amusoft.CodeAnalysis.Analyzers/blob/master/LICENSE for details
+
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Testing.MSTest;
 using Microsoft.CodeAnalysis.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.CodeAnalysis.Testing.DiagnosticResult;
+using Verifier = Microsoft.CodeAnalysis.CSharp.Testing.MSTest.CodeFixVerifier<Microsoft.CodeAnalysis.Testing.EmptyDiagnosticAnalyzer,
+		Amusoft.CodeAnalysis.Analyzers.CS0123.FixByIntroducingMethod>;
 
 namespace Amusoft.CodeAnalysis.Analyzers.Test.Tests.CS0123
 {
 	[TestClass]
-	public class FixByMethodIntroductionTests : TestHelper.CodeFixVerifier
+	public class FixByMethodInsertionTests
 	{
 		[TestMethod]
-		public async Task NoAction()
+		public async Task EmptySourceNoAction()
 		{
-			await CodeFixVerifier<EmptyDiagnosticAnalyzer, FixByIntroducingMethod>.VerifyCodeFixAsync(string.Empty, string.Empty);
+			await Verifier.VerifyCodeFixAsync(string.Empty, string.Empty);
 		}
 
 		[TestMethod]
 		public async Task DiagnosticAtObjectCreationExpression()
 		{
+			
 			var test = @"
 using System;
 using System.Collections.Generic;
@@ -32,7 +39,7 @@ namespace ConsoleApplication1
     {
         TypeName()
         {
-            var action = new Func<string, int>(TestMethod);
+            var action = new Func<int, string>(TestMethod);
         }
 
         private int TestMethod(int arg)
@@ -57,23 +64,21 @@ namespace ConsoleApplication1
     {
         TypeName()
         {
-            var action = new Func<string, int>(TestMethod);
+            var action = new Func<int, string>(TestMethod);
         }
 
-        private int TestMethod(int arg)
-        {
-            throw new NotImplementedException();
-        }
-
-        private int TestMethod(string arg)
+        private string TestMethod(int arg)
         {
             throw new NotImplementedException();
         }
     }
 }";
-			var diagnostic1 = DiagnosticResult.CompilerError("CS0123").WithLocation(15, 26);
+			var diagnostics = new[]
+			{
+				CompilerError("CS0407").WithLocation(15, 48),
+			};
 
-			await CodeFixVerifier<EmptyDiagnosticAnalyzer, FixByIntroducingMethod>.VerifyCodeFixAsync(test, diagnostic1, fixtest);
+			await Verifier.VerifyCodeFixAsync(test, diagnostics, fixtest);
 		}
 	}
 }
--- src/Amusoft.CodeAnalysis.Analyzers/CS0123/FixByIntroducingMethod.cs
+++ src/Amusoft.CodeAnalysis.Analyzers/CS0123/FixByIntroducingMethod.cs
@@ -2,23 +2,17 @@
 // // This file is a part of Amusoft.CodeAnalysis.Analyzers and is licensed under Apache 2.0
 // // See https://github.com/taori/Amusoft.CodeAnalysis.Analyzers/blob/master/LICENSE for details
 
-using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using System.Composition;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Amusoft.CodeAnalysis.Analyzers.Shared;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Simplification;
 
-namespace Amusoft.CodeAnalysis.Analyzers.CodeGeneration.CS0123
+namespace Amusoft.CodeAnalysis.Analyzers.CS0123
 {
-	[ExportCodeFixProvider(LanguageNames.CSharp, Name = "CS0123-FixByReplacingMethodReturnValue"), Shared]
-	public class FixByReplacingMethodReturnValue : CodeFixProviderBase
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = "CS0123-FixByIntroducingMethod"), Shared]
+	public class FixByIntroducingMethod : CodeFixProviderBase
 	{
 		/// <inheritdoc />
 		protected override string DiagnosticId { get; } = "CS0123";
@@ -26,7 +20,7 @@ namespace Amusoft.CodeAnalysis.Analyzers.CodeGeneration.CS0123
 		/// <inheritdoc />
 		protected override string GetEquivalenceKey(SyntaxNode rootNode)
 		{
-			return "CS0123-FixByReplacingMethodReturnValue";
+			return "CS0123-FixByIntroducingMethod";
 		}
 
 		/// <inheritdoc />
@@ -34,7 +28,7 @@ namespace Amusoft.CodeAnalysis.Analyzers.CodeGeneration.CS0123
 		{
 			var member = GetAnnotationValue(rootNode, MemberAnnotation);
 			var typeName = GetAnnotationValue(rootNode, TypeAnnotation);
-			return string.Format(Resources.MessageFormat_CS0123_FixByReplacingMethodReturnValue_0_1, member, typeName);
+			return string.Format(Resources.MessageFormat_CS0407_FixMethodReturnType_0_1, member, typeName);
 		}
 
 		/// <inheritdoc />
