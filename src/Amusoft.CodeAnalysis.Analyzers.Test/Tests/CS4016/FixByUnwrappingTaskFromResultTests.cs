1d77ddb9366e873ee652899dbe022617d3855e41
Andreas Müller <taori@users.noreply.github.com>
2020-01-21 15:49:00 +0100

    Features/codefix 9 cs4016 (#20)
    * initial codefix + test
    
    * feature works

---
+++ src/Amusoft.CodeAnalysis.Analyzers.Test/Tests/CS4016/FixByUnwrappingTaskFromResultTests.cs
@@ -0,0 +1,161 @@
+﻿// Copyright 2020 Andreas Müller
+// This file is a part of Amusoft.CodeAnalysis.Analyzers and is licensed under Apache 2.0
+// See https://github.com/taori/Amusoft.CodeAnalysis.Analyzers/blob/master/LICENSE for details
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Testing.MSTest;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Microsoft.CodeAnalysis.Testing.DiagnosticResult;
+using Verifier =
+	Microsoft.CodeAnalysis.CSharp.Testing.MSTest.CodeFixVerifier<Microsoft.CodeAnalysis.Testing.EmptyDiagnosticAnalyzer,
+		Amusoft.CodeAnalysis.Analyzers.CS4016.FixByUnwrappingTaskFromResult>;
+
+namespace Amusoft.CodeAnalysis.Analyzers.Test.Tests.CS4016
+{
+	[TestClass]
+	public class FixByUnwrappingTaskFromResultTests
+	{
+		[TestMethod]
+		public async Task EmptySourceNoAction()
+		{
+			await Verifier.VerifyCodeFixAsync(string.Empty, string.Empty);
+		}
+
+		[TestMethod]
+		public async Task FixMethod()
+		{
+			var test = @"
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class TestClass
+{
+	private string _workspaceFile;
+
+	public async Task<Configuration[]> LoadStorageContentAsync()
+	{
+		var fileInfo = new FileInfo(_workspaceFile);
+		if (!fileInfo.Exists)
+		{
+			Log.Error($""No configuration storage located at {fileInfo.FullName}."");
+			return Task.FromResult(Array.Empty<Configuration>());
+		}
+
+		using (var stream = new StreamReader(new FileStream(fileInfo.FullName, FileMode.Open)))
+		{
+			try
+			{
+				var storage = new Storage();
+				return Task.FromResult(storage.Configurations.ToArray());
+			}
+			catch (Exception e)
+			{
+				Log.Error(e);
+				return Task.FromResult(Array.Empty<Configuration>());
+			}
+		}
+	}
+
+	public class Log
+	{
+		public static void Error(Exception p0)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void Error(string p0)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class Storage
+	{
+		public Configuration[] Configurations { get; set; }
+	}
+
+	public class Configuration
+	{
+		public Guid Id { get; set; }
+		public string ConfigurationName { get; set; }
+	}
+}";
+
+			var fixtest = @"
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class TestClass
+{
+	private string _workspaceFile;
+
+	public async Task<Configuration[]> LoadStorageContentAsync()
+	{
+		var fileInfo = new FileInfo(_workspaceFile);
+		if (!fileInfo.Exists)
+		{
+			Log.Error($""No configuration storage located at {fileInfo.FullName}."");
+			return Array.Empty<Configuration>();
+		}
+
+		using (var stream = new StreamReader(new FileStream(fileInfo.FullName, FileMode.Open)))
+		{
+			try
+			{
+				var storage = new Storage();
+				return storage.Configurations.ToArray();
+			}
+			catch (Exception e)
+			{
+				Log.Error(e);
+				return Array.Empty<Configuration>();
+			}
+		}
+	}
+
+	public class Log
+	{
+		public static void Error(Exception p0)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void Error(string p0)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class Storage
+	{
+		public Configuration[] Configurations { get; set; }
+	}
+
+	public class Configuration
+	{
+		public Guid Id { get; set; }
+		public string ConfigurationName { get; set; }
+	}
+}";
+			var diagnostics = new[]
+			{
+				// Test0.cs(17,11): error CS4016: Since this is an async method, the return expression must be of type 'TestClass.Configuration[]' rather than 'Task<TestClass.Configuration[]>'
+				DiagnosticResult.CompilerError("CS4016").WithSpan(17, 11, 17, 56)
+					.WithArguments("TestClass.Configuration[]"),
+// Test0.cs(25,12): error CS4016: Since this is an async method, the return expression must be of type 'TestClass.Configuration[]' rather than 'Task<TestClass.Configuration[]>'
+				DiagnosticResult.CompilerError("CS4016").WithSpan(25, 12, 25, 61)
+					.WithArguments("TestClass.Configuration[]"),
+// Test0.cs(30,12): error CS4016: Since this is an async method, the return expression must be of type 'TestClass.Configuration[]' rather than 'Task<TestClass.Configuration[]>'
+				DiagnosticResult.CompilerError("CS4016").WithSpan(30, 12, 30, 57)
+					.WithArguments("TestClass.Configuration[]")
+			};
+
+			await Verifier.VerifyCodeFixAsync(test, diagnostics, fixtest);
+		}
+	}
+}
---
+++ src/Amusoft.CodeAnalysis.Analyzers/CS4016/FixByUnwrappingTaskFromResult.cs
@@ -0,0 +1,70 @@
+﻿// Copyright 2020 Andreas Müller
+// This file is a part of Amusoft.CodeAnalysis.Analyzers and is licensed under Apache 2.0
+// See https://github.com/taori/Amusoft.CodeAnalysis.Analyzers/blob/master/LICENSE for details
+
+using System;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amusoft.CodeAnalysis.Analyzers.Shared;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Simplification;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Amusoft.CodeAnalysis.Analyzers.CS4016
+{
+	[ExportCodeFixProvider(LanguageNames.CSharp,
+		 Name = "Amusoft.CodeAnalysis.Analyzers.CS4016-FixByUnwrappingTaskFromResult"), Shared]
+	public class FixByUnwrappingTaskFromResult : SingleDiagnosticDocumentCodeFixProviderBase
+	{
+		/// <inheritdoc />
+		protected override string DiagnosticId { get; } = "CS4016";
+
+		/// <inheritdoc />
+		protected override string GetEquivalenceKey(SyntaxNode rootNode)
+		{
+			return $"{DiagnosticId}-FixByUnwrappingTaskFromResult";
+		}
+
+		/// <inheritdoc />
+		protected override string GetTitle(SyntaxNode rootNode)
+		{
+			return Resources.MessageFormat_CS4016_FixByUnwrappingTaskFromResult;
+		}
+
+		/// <inheritdoc />
+		protected override async Task<Document> GetFixedDiagnosticAsync(Document document, TextSpan span,
+			CancellationToken cancellationToken)
+		{
+			var syntaxRoot = await document.GetSyntaxRootAsync(cancellationToken)
+				.ConfigureAwait(false);
+			var diagnosticNode = syntaxRoot.FindNode(span);
+
+			if (diagnosticNode is InvocationExpressionSyntax invocationExpression
+			    && invocationExpression.Expression is MemberAccessExpressionSyntax memberAccessExpression
+			    && memberAccessExpression.Name.Identifier.Text.Equals("FromResult")
+			    && memberAccessExpression.Expression is IdentifierNameSyntax nameSyntax
+			    && nameSyntax.Identifier.Text.Equals("Task"))
+			{
+				var argumentSyntax = invocationExpression.ArgumentList.Arguments.FirstOrDefault();
+				if (argumentSyntax != null)
+				{
+					var replaced = syntaxRoot.ReplaceNode(invocationExpression,
+						argumentSyntax.Expression);
+
+					return document.WithSyntaxRoot(replaced);
+				}
+			}
+
+			return document;
+		}
+	}
+}
